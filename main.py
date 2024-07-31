452875d36d4c37e4edb82abadeeba3f29eeb5a89
JakobEriksen123 <96021450+JakobEriksen123@users.noreply.github.com>
2024-07-31 21:27:53 +0200

    added my own print

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
 print("something new 2")
 plt.show()
-
+print("This is my line")
 print("something new")
 
 print("I love to make changes without coordinating")
